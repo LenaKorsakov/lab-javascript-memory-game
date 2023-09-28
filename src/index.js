aeaf0be545e98b9bd4f3aa8d03c1c2b2619370a7
ElenaKorsakova <lenawwka@gmail.com>
2023-09-28 16:57:39 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { MemoryGame } from './memory.js';
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -22,12 +24,17 @@ const cards = [
   { name: 'spiderman', img: 'spiderman.jpg' },
   { name: 'superman', img: 'superman.jpg' },
   { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
+  { name: 'thor', img: 'thor.jpg' },
 ];
 
 const memoryGame = new MemoryGame(cards);
 
+const pairsClicked = document.querySelector('#pairs-clicked');
+const pairsGuessed = document.querySelector('#pairs-guessed');
+const endGameScreen = document.querySelector('.end-game-screen');
+
 window.addEventListener('load', (event) => {
+  memoryGame.shuffleCards();
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -43,9 +50,57 @@ window.addEventListener('load', (event) => {
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+    card.addEventListener('click', flipCard);
   });
 });
+
+function flipCard() {
+  if (!memoryGame.isTwoCardsPicked()) {
+    this.classList.add('turned');
+    memoryGame.addToPicked(this);
+  }
+
+  if (memoryGame.isTwoCardsPicked()) {
+    const [firstCard, secondCard] = memoryGame.pickedCards;
+    checkForMatch(firstCard, secondCard);
+    pairsGuessed.textContent = memoryGame.pairsGuessed;
+  }
+  pairsClicked.textContent = memoryGame.pairsClicked;
+}
+
+function checkForMatch(firstCard, secondCard) {
+  const isPair = memoryGame.checkIsPair(
+    firstCard.dataset.cardName,
+    secondCard.dataset.cardName
+  );
+
+  if (isPair) {
+    disableCards(firstCard, secondCard);
+    memoryGame.resetPickedCards();
+
+    if (memoryGame.checkIsFinished()) {
+      endGame();
+    }
+  } else {
+    unflipCards(firstCard, secondCard);
+    memoryGame.resetPickedCards();
+  }
+}
+
+function unflipCards(firstCard, secondCard) {
+  setTimeout(() => {
+    firstCard.classList.remove('turned');
+    secondCard.classList.remove('turned');
+  }, 1000);
+}
+
+function disableCards(firstCard, secondCard) {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function endGame() {
+  if (memoryGame.checkIsFinished()) {
+    endGameScreen.showModal();
+  }
+}
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,48 @@
-class MemoryGame {
+export class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    //optimized version of Fisher-Yates:
+    if (!this.cards) {
+      return undefined;
+    }
+
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+
+    return this.cards;
   }
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
+  checkIsPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  checkIfFinished() {
-    // ... write your code here
+  checkIsFinished() {
+    return this.cards.length / 2 === this.pairsGuessed ? true : false;
+  }
+
+  addToPicked(name) {
+    this.pickedCards.push(name);
+  }
+
+  isTwoCardsPicked() {
+    return this.pickedCards.length === 2 ? true : false;
+  }
+
+  resetPickedCards() {
+    this.pickedCards = [];
   }
 }
